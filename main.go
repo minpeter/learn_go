81ccbcd33b8f246f992c21bcb4fb05f8a1cf2db6
minpeter <kali2005611@gmail.com>
2021-12-30 09:16:57 +0900

    Dictonary Project complete

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/minpeter/learngo/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word", "second": "Second word"}
-	definition, err := dictionary.Search("fir")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
-	}
+	
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -1,11 +1,18 @@
 package mydict
 
-import "errors"
+import (
+	"errors"
+)
 
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not found")
+
+var (
+	errNotFound = errors.New("not found")
+	errCantUpdate = errors.New("can't update non-existent word")
+	errWordExist = errors.New("word already exist")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +21,34 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = definition
+	case nil:
+		return errWordExist
+	}
+	return nil
+}
+
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+//Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
