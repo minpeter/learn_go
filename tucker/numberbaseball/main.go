0648cb329fc4f015a15cf5fc704e80521fae12c0
minpeter <kali2005611@gmail.com>
2022-02-23 22:11:31 +0900

    numberbaseball first error fix (TDD)

--- tucker/numberbaseball/main.go
+++ tucker/numberbaseball/main.go
@@ -26,5 +26,32 @@ func main() {
 		}
 	}
 	//걸린 회수 출력
-	fmt.Print("%d회 만에 맞추셨습니다.\n", cnt)
+	fmt.Printf("%d 회 만에 맞추셨습니다.\n", cnt)
+}
+
+func MakeNumbers() [3]int {
+	// 0~9 사이에 겹치지 않는 무작위 숫자 3개 반환
+	var rst [3]int
+	return rst
+}
+
+func InputNumbers() [3]int {
+	// 키보드로 숫자 3개 입력 받아서 배열 반환
+	var rst [3]int
+	return rst
+}
+
+func CompareNumbers(numbers, inputNumbers [3]int) bool {
+	// 두개의 숫자 3개를 비교해서 결과를 반환
+	return true
+}
+
+func PrintResult(result bool) {
+	// 결과 출력
+	fmt.Println(result)
+}
+
+func IsGameEnd(result bool) bool {
+	// 게임 종료 여부 반환
+	return true
 }
